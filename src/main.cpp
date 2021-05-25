5f439b6fb3de4c2ac906f062aeffbeb23994140a
Alkoarism <alkoarism@gmail.com>
2021-05-24 22:31:32 -0300

    Included all the basic settings to open a named and colored window.

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,73 @@
 
 #include <iostream>
 
-int main() {
-	std::cout << "This is the beggining of something big." << std::endl;
-	std::cin.get();
+// function declarations ------------------------------------------------------
+void processInput(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+// global variables -----------------------------------------------------------
+const int screenWidth = 800, screenHeight = 600;
+
+int main() {
+	// glfw: initialize and configure --------------------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// glfw: window creation and setup -------------------------------------------
+	GLFWwindow* window = glfwCreateWindow
+		(screenWidth,  screenHeight, "OpenGL standard template", NULL, NULL);
+	if (!window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// glfw: setup
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad: load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// vertices definition -------------------------------------------------------
+	// objects and buffer configurations -----------------------------------------
+	// texture handling ----------------------------------------------------------
+	// initialization before rendering -------------------------------------------
+	// render loop (happens every frame) -----------------------------------------
+	while (!glfwWindowShouldClose(window)) {
+		// -> frame time tracker
+		// -> input handling
+		processInput(window);
+
+		// -> rendering commands and configuration
+		glClearColor(0.5f, 0.5f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// ---> texture configurations
+		// ---> coordinate configurations
+		// -> check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
